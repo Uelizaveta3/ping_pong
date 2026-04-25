73c1a8d78bcb1a72d2089445a70eb21e0ccd0f24
Елизавета Буторина <155951551+Uelizaveta3@users.noreply.github.com>
2026-04-25 17:57:53 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -3,9 +3,7 @@ from pygame import *
 main_win = display.set_mode((700,500))
 display.set_caption('Пинг Понг')
 
-# background = transform.scale(
-#     image.load('galaxy.jpg')
-# , (700,500))
+main_win.fill((0, 255, 255))
 
 class GameSprite(sprite.Sprite):
     def __init__ (self, speed, x, y, img, size=(65,70)):
@@ -22,15 +20,36 @@ class GameSprite(sprite.Sprite):
         main_win.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 620:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 480:
+            self.rect.y += self.speed
+    def update_r(self):
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 480:
+            self.rect.y += self.speed
+
+
+#шрифты
+font.init()
+lose_1 = font.render(
+    'игрок 1 проиграл', True, (139, 0, 0)
+)
+
+lose_2 = font.render(
+    'игрок 2 проиграл', True, (139, 0, 0)
+)
+
+#звуки
+mixer.init()
 
 
 game = True
 
 while game:
-    pass
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
